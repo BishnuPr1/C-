63f60e1819b7c60a163e267f211ad039ad4a8822
Bishnu Prasad Patnaik <83540949+BishnuPr1@users.noreply.github.com>
2021-08-28 23:35:08 +0530

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a; 
+    a=12;
+
+    cout<<"int size"<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"float size"<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"char size"<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"bool size"<<sizeof(d)<<endl;
+}
